e60e8bc5c7c2d2e65b6ddd14e7d8ab0a7e42ccc8
Lukasz Pelczar <lukasxxz@o2.pl>
2022-07-10 16:18:14 +0200

    production config update

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import * as express from "express";
-import * as cors from 'cors';
+import express from "express";
+import cors from 'cors';
 import 'express-async-errors';
 import { ebookRouter } from "./routers/ebook";
 import { handleError } from "./utils/errors";

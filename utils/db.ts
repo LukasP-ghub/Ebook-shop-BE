b4fe81e97bec69182c77d331e6e2d8a2f31b4bfe
Lukasz Pelczar <lukasxxz@o2.pl>
2022-07-10 16:56:07 +0200

    add db password

--- utils/db.ts
+++ utils/db.ts
@@ -4,6 +4,7 @@ require('dotenv').config();
 export const pool = createPool({
     host: process.env.HOST,
     user: process.env.USER,
+    password: process.env.PASSWORD,
     database: process.env.DATABASE,
     namedPlaceholders: true,
     decimalNumbers: true,

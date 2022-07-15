8e6f28f78b88be71e8cfbffb10fdfad2d05c5394
Lukasz Pelczar <lukasxxz@o2.pl>
2022-07-15 21:28:45 +0200

    extract users controllers

--- routers/user.ts
+++ routers/user.ts
@@ -1,61 +1,14 @@
 import { Router } from "express";
-import { ValidationError } from "../utils/errors";
-import { UserRecord } from "../records/user.record";
-import { v4 as uuidv4 } from 'uuid';
+import { findUser, register, signIn } from "../controllers/user.controller";
 const bcrypt = require('bcrypt');
 
 
 export const userRouter = Router();
 
-userRouter // /ebooks
+userRouter // /users
 
-  .post("/signin", async (req, res) => {
-    try {
-      const user = await UserRecord.findOne({ email: req.body.email });
-      if (user) {
-        //if password matches
-        if (bcrypt.compareSync(req.body.password, user.password)) {
-          res.status(200).json({
-          });
-          return;
-        }
-      }
-      res.status(401).json({ message: "Invalid email or password" });
-    } catch (err) {
-      res.json({ message: "Sorry, something gone wrong" });
-    }
-  })
+  .post("/signin", signIn)
 
-  .post("/register", async (req, res) => {
-    try {
-      //if email already exists
-      const existingEmail = await UserRecord.findOne({ email: req.body.email });
-      if (existingEmail) throw new ValidationError("Podany adres email już istnieje.");
+  .post("/register", register)
 
-      //if not
-      //new user
-      const user = await UserRecord.save({
-        user_id: uuidv4(),
-        email: req.body.email,
-        password: bcrypt.hashSync(req.body.password, 8),
-      });
-
-      res.status(201).json({ createdUser: user });
-    } catch (err) {
-      console.log(err);
-      res.json(err.message);
-    }
-  })
-
-  .get("/:id", async (req, res) => {
-    try {
-      const user = await UserRecord.findById({ id: req.params.id });
-      if (user) {
-        res.status(200).json(user);
-      } else {
-        res.status(404).json({ message: "User not found" });
-      }
-    } catch (err) {
-      res.status(500).json({ message: err.message });
-    }
-  })
+  .get("/:id", findUser)

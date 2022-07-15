e64a59426fb87922b2eea045967f50e1be95a98c
Lukasz Pelczar <lukasxxz@o2.pl>
2022-07-15 15:51:09 +0200

    extract controllers, async handler package

--- routers/ebook.ts
+++ routers/ebook.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { getAll, getMany, getOne } from "../controllers/ebook.controller";
 import { EbookRecord } from "../records/ebook.record";
 import { EbookDBSearchKey, EbookDBSearchOptions, ListEbooksRes } from "../types";
 import { ValidationError } from "../utils/errors";
@@ -7,34 +8,8 @@ export const ebookRouter = Router();
 
 ebookRouter // /ebooks
 
-    .get('/', async (req, res) => {
-        const ebookList = await EbookRecord.getMany();
+    .get('/', getAll)
 
-        res.json({
-            ebookList,
-        });
-    })
+    .get('/get-many', getMany)
 
-    .get('/get-many', async (req, res) => {
-        const { query } = req;
-        const allowedSearchKeys: string[] = Object.values(EbookDBSearchKey);
-        if (query?.key && !allowedSearchKeys.includes(`${query?.key}`)) throw new ValidationError('Niepoprawny klucz wyszukiwania');
-
-        const searchOptions: EbookDBSearchOptions = req.query;
-
-        const ebookList = await EbookRecord.getManyBy(searchOptions);
-
-        res.json({
-            ebookList,
-        });
-    }
-    )
-
-    .get('/get-one', async (req, res) => {
-        const ebook_id = `${req.query?.ebook_id}`;
-        const ebook = await EbookRecord.getOneById(ebook_id);
-
-        res.json({
-            ebook,
-        });
-    })
+    .get('/get-one', getOne)

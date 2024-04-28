43a152f25d47b158d7f54db6885ebb64f9d0bc56
Lukasz Pelczar <lukasxxz@o2.pl>
2024-04-28 15:16:18 +0200

    typeorm config

--- src/config/typeOrm.config.ts
+++ src/config/typeOrm.config.ts
@@ -1,16 +1,35 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-export const TYPEORM_CONFIG: TypeOrmModuleOptions = {
+const TYPEORM_CONFIG: TypeOrmModuleOptions = {
   type: 'mysql',
   host: 'localhost',
   port: 3306,
   username: 'root',
   password: '',
-  //database: process.env.NODE_ENV === 'test' ? 'wobclone_test' : 'wobclone',
-  database: 'wobclone_test',
-  //entities: ['dist/**/**.entity{.ts,.js}'],
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   bigNumberStrings: false,
   logging: true,
   synchronize: false,
 };
+
+switch (process.env.NODE_ENV) {
+  case 'test':
+    Object.assign(TYPEORM_CONFIG, {
+      database: 'wobclone_test',
+    });
+    break;
+  case 'development':
+    Object.assign(TYPEORM_CONFIG, {
+      database: 'wobclone',
+    });
+    break;
+  case 'production':
+    Object.assign(TYPEORM_CONFIG, {
+      database: 'wobclone',
+    });
+    break;
+  default:
+    throw new Error('No database specified for this environment');
+}
+
+export default TYPEORM_CONFIG;
